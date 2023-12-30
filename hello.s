52560140e78ffb7bcb458187589a618c97da56fa
Brook Jeynes <jeynesbrook@gmail.com>
2023-12-30 18:56:05 +1000

    first commit

---
+++ hello.s
@@ -0,0 +1,25 @@
+// --
+// Assembler program to print "Hello, world!"
+// to standard output.
+// --
+
+// Define `_start` as a global symbol so the linker can
+// find it
+.global _start
+
+// Starting point of the application
+_start:
+    // write(int fd, const void *buf, size_t count)
+    mov    x0, #1          // fd = STDOUT_FILENO
+    ldr    x1, =msg        // buf = msg
+    mov    x2, #14         // count = len
+    mov    x8, #64         // `write()` is syscall #64
+    svc    #0              // Invoke syscall
+
+    // exit(int status)
+    mov    x0, #0     // status = 0
+    mov    x8, #93    // exit is syscall #93
+    svc    #0         // invoke syscall
+
+.data
+msg: .ascii "Hello, world!\n"
